acd3371c6d38df2ce0505eac16f144da9450cc42
Daniil <danielfalcony@gmail.com>
2023-10-27 23:51:34 +0300

    in progress

--- RecipeWebsite/recipeapp/models.py
+++ RecipeWebsite/recipeapp/models.py
@@ -7,10 +7,15 @@ class Author(models.Model):
     email = models.EmailField()
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(default='', blank=True)
     cooking_steps = models.TextField(default='', blank=True)
     cooking_time = models.TimeField()
     img = models.ImageField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.SET_DEFAULT, default='Author unknown')
+    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default='Category unknown')
