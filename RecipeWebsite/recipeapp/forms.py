2555ec4b092fb7608734cca01c43620faea0a087
letusbeus <letusbeus@mail.ru>
2023-11-04 01:53:56 +0300

    Updated forms.RecipeEditForm (added labels), forms.RecipeAddForm (classes added)

--- RecipeWebsite/recipeapp/forms.py
+++ RecipeWebsite/recipeapp/forms.py
@@ -17,19 +17,30 @@ class SignUpForm(UserCreationForm):
 
 class RecipeAddForm(forms.Form):
     recipe_name = forms.CharField(label='Название рецепта:', max_length=100, required=True, widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'Введите название рецепта'}))
+        attrs={'class': 'recipe_name', 'placeholder': 'Введите название рецепта'}))
     recipe_description = forms.CharField(label='Описание рецепта:', widget=forms.Textarea(
-        attrs={'placeholder': 'Введите описание рецепта'}))
+        attrs={'class': 'recipe_description', 'placeholder': 'Введите описание рецепта'}))
     recipe_cooking_steps = forms.CharField(label='Шаги приготовления:', required=True, widget=forms.Textarea(
-        attrs={'placeholder': 'Введите шаги приготовления.'}))
-    recipe_cooking_time = forms.IntegerField(label='Введите время приготовления в минутах:', min_value=1, required=True,
-                                             widget=forms.NumberInput(attrs={'class': 'form-control'}))
-    recipe_category = forms.CharField(label='Категория рецепта', widget=forms.TextInput(
-        attrs={'placeholder': 'Укажите категорию рецепта:'}))
-    recipe_image = forms.ImageField(label='Изображение рецепта:')
+        attrs={'class': 'cooking_steps', 'placeholder': 'Введите шаги приготовления.'}))
+    recipe_cooking_time = forms.IntegerField(label='Время приготовления (мин):', min_value=1, required=True,
+                                             widget=forms.NumberInput(attrs={
+                                                 'class': 'cooking_time',
+                                                 'placeholder': 'Введите время приготовления в минутах'}))
+    recipe_category = forms.CharField(label='Категория рецепта:', widget=forms.TextInput(
+        attrs={'class': 'recipe_category', 'placeholder': 'Укажите категорию рецепта:'}))
+    recipe_image = forms.ImageField(label='Изображение рецепта:',
+                                    widget=forms.ClearableFileInput(attrs={'class': 'recipe_image'}),)
 
 
 class RecipeEditForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ['name', 'description', 'cooking_steps', 'cooking_time', 'img', 'category']
+        labels = {
+            'name': 'Название рецепта:',
+            'description': 'Описание рецепта:',
+            'cooking_steps': 'Шаги приготовления',
+            'cooking_time': 'Время приготовления',
+            'img': 'Изображение рецепта',
+            'category': 'Категория рецепта',
+        }
