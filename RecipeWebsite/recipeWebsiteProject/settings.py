4a7192d5a749a9b17e40c14b1da4ccd9b1af053f
Daniil <danielfalcony@gmail.com>
2023-11-04 14:24:15 +0300

    Ready project

--- RecipeWebsite/recipeWebsiteProject/settings.py
+++ RecipeWebsite/recipeWebsiteProject/settings.py
@@ -20,13 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&je47=z^j+0k_^r!!s&=9jchd&2yysuu$y&xc!jpj23%nx84@o'
+# SECRET_KEY = 'django-insecure-&je47=z^j+0k_^r!!s&=9jchd&2yysuu$y&xc!jpj23%nx84@o'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
-
+    '127.0.0.1',
+    'danielfalcony.pythonanywhere.com',
 ]
 
 LOGIN_URL = '/login/'  # ссылка на форму для логина
@@ -81,8 +86,15 @@ WSGI_APPLICATION = 'recipeWebsiteProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'danielfalcony$danielfalcony',
+        'USER': 'danielfalcony',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'danielfalcony.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4'; SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -122,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
